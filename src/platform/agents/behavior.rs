7dc71c9f1d2f535a283b1093889152803eec7761
Daniel Rojas <159dram@gmail.com>
2023-10-10 01:12:41 -0600

    default behavior trait impl added

--- src/platform/agents/behavior.rs
+++ src/platform/agents/behavior.rs
@@ -1,10 +1,10 @@
 pub trait Behavior {
-    fn setup(&mut self);
-    fn done(&mut self) -> bool;
-    fn action(&mut self);
-    fn failure_detection(&mut self) -> bool;
-    fn failure_identification(&mut self);
-    fn failure_recovery(&mut self);
+    fn setup(&mut self){print!("no setup implemented")}
+    fn done(&mut self) -> bool {false}
+    fn action(&mut self) {print!("no action implemented")}
+    fn failure_detection(&mut self) -> bool {true}
+    fn failure_identification(&mut self){print!("no failure identification implemented")}
+    fn failure_recovery(&mut self){print!("no failure recovery implemented")}
 }
 
 pub fn execute(mut behavior: impl Behavior) {
