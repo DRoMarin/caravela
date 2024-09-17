2b6736892c18cce64d3eadfcb1c2e6768c79ba1e
Daniel Rojas <159dram@gmail.com>
2024-09-17 00:14:47 -0600

    add agent creation macros

--- caravela/src/utils.rs
+++ caravela/src/utils.rs
@@ -30,3 +30,51 @@ macro_rules! caravela_dflt {
         println!("[DEFAULT] {}",format_args!($($arg)*));
     }};
 }
+
+#[macro_export]
+macro_rules! agent{
+    ($vis: vis $agent: ident) => {
+        #[derive(Debug)]
+        $vis struct $agent(Agent);
+        impl AgentBuild for $agent {
+            fn agent_builder(__0: Agent) -> $agent {
+                $agent(__0)
+            }
+        }
+
+        impl AgentBase for $agent {
+            fn agent(&mut self) -> &mut Agent{
+                &mut self.0
+            }
+        }
+    };
+}
+
+//($vis: vis $agent: ident $(< $( $gen_lt:tt $( : $fbd:tt $(+ $sbd:tt )* )? ),+ >)? ) => {
+
+#[macro_export]
+macro_rules! agent_with_param {
+    //($vis: vis $agent: ident, $pvis: vis $pty:ident $(< $( $glt:tt $(:$fbd:tt $(+ $sbd:tt )* )? ),+ >)? ) => {
+    //todo!();
+    //};
+    ($vis: vis $agent: ident, $param_vis: vis $param_ty:ty) => {
+        #[derive(Debug)]
+        $vis struct $agent(Agent,$param_ty);
+
+        impl AgentBuildParam for $agent {
+            type Parameter = $param_ty;
+            fn agent_with_param_builder(__0: Agent, __1: $param_ty) -> $agent {
+                $agent(__0,__1)
+            }
+            fn param(&mut self) -> &mut $param_ty{
+                &mut self.1
+            }
+        }
+
+        impl AgentBase for $agent {
+            fn agent(&mut self) -> &mut Agent{
+                &mut self.0
+            }
+        }
+    };
+}
